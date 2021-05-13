2cf1a3deb50ce96fb4370bcf0ee77a3f13c15db5
Li-bear <allisoncastellanos8@gmail.com>
2021-05-12 21:03:49 -0500

    Cambio de nombres de los archivos del menu lateral Agregar la opcion de "Acerca de" en el menu Cambiar los nombres de las opciones del menu lateral

--- app/src/main/java/com/example/barra/ui/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/example/barra/ui/statistics/StatisticsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.barra.ui.slideshow
+package com.example.barra.ui.statistics
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class StatisticsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Estadísticas"
     }
     val text: LiveData<String> = _text
 }
